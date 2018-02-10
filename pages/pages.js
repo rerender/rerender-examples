6cd4a83901904e03d432c9c73f4419ecaca61be8
Vasiliy Lector <vasiliy.lector@gmail.com>
2018-02-10 09:22:37 +0300

    fix first start

--- pages/pages.js
+++ pages/pages.js
@@ -2,7 +2,6 @@ export { default as Index } from './Index';
 export { default as Stream } from './Stream';
 export { default as Controllers } from './Controllers';
 export { default as Sierpinsky } from './Sierpinsky';
-export { default as Second } from './Second';
 export { default as Forms } from './Forms';
 export { default as Forms2 } from './Forms2';
 export { default as Todos } from './Todos';
